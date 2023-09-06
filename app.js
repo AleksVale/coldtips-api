a7d9c002004d6fc1cff24c418a937d45a5b9d83a
AleksVale <aleksander.ribeiro@sga.pucminas.br>
2023-09-06 18:41:32 -0300

    upload build

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ import express from 'express';
 import userRoutes from './src/user/user-routes.js';
 import authRoutes from './src/auth/auth-routes.js';
 import { errorHandler } from './src/middlewares/errorHandler.js';
-import bodyParser from 'body-parser';
 import { middlewareValidarJWT } from './src/middlewares/authMiddleware.js';
 
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 app.use('/api/users',middlewareValidarJWT, userRoutes);
 app.use('/api/auth', authRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.use(errorHandler);
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen();
