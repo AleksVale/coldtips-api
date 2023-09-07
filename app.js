8c0a752625c64c5c6e2b631a1056d7c944b7f007
AleksVale <aleksander.ribeiro@sga.pucminas.br>
2023-09-06 22:46:03 -0300

    back jwt middleware

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = Number(process.env.PORT) || 3000;
 app.use(cors());
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use('/api/users',middlewareValidarJWT, userRoutes);
 app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('Hello World!');
