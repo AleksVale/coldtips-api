ca508b119ab9fe13aff24e9d0dc07612b114b066
AleksVale <aleksander.ribeiro@sga.pucminas.br>
2023-09-07 18:34:32 -0300

    paycontroller

--- src/perfectpay/perfectpay-controller.js
+++ src/perfectpay/perfectpay-controller.js
@@ -4,7 +4,7 @@ export async function webhook(req, res, next) {
   try {
     const request = req.body;
     await createUserWebhook(request);
-    res.status(200).send();
+    res.status(200).send({data: 'WebHook tratado com sucesso'});
   } catch (error) {
     next(error);
   }
